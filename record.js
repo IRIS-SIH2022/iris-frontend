e4714f77965d3753a8c3c29d66458fe37268809b
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-08-25 11:14:52 +0530

    Add: api route in record

--- record.js
+++ record.js
@@ -5,4 +5,21 @@ document.getElementById("record-form").addEventListener("submit", async (e) => {
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
   console.log(formProps);
+  // post request to api
+  const request = await fetch("http://127.0.0.1:8000/marker/add", {
+    method: "POST",
+    body: JSON.stringify(formProps),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  // check response and show message
+  // const response = await request.json();
+  // console.log(response);
+  // // alert if not 201
+  // if (response.status !== 201) {
+  //   alert("Error occured while adding record");
+  // }
+  // clear form
+  document.getElementById("record-form").reset();
 });
